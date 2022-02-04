397354ac05ff5ca0ded540ad796a24fe25f9aa6d
Imane El mohalil <elmohalilimane@gmail.com>
2022-02-04 22:09:54 +0100

    shupershmollpeepee

---
+++ main.py
@@ -0,0 +1,15 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    text = question["text"]
+    answer = question["answer"]
+    new_q = Question(text,answer)
+    question_bank.append(new_q)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions() == True:
+    quiz.next_question()
---
+++ question_model.py
@@ -0,0 +1,10 @@
+from cgitb import text
+
+
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+# question = Question("What is Your name ?","Imane")
+# print(question.text)
